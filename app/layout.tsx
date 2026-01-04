5e4bfbe08faf6537859d209702f4157fe0bc4723
Lien <lk.tran14@gmail.com>
2026-01-04 20:59:10 +0000

    Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,21 +25,8 @@ export const metadata: Metadata = {
   description: "Highlights from all around the world",
   generator: "v0.app",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/IMG_0195.png",
+    apple: "/IMG_0195.png",
   },
 }
 
